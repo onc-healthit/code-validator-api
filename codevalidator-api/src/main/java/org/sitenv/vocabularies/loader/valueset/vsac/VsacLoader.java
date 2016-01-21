a29dc346425560ea81774a11e51a761c0540e79c
Brian Harris <sendbrianemail@gmail.com>
2016-01-20 22:40:28 -0500

    Created validator to handle UCUM valueset. Updated VSAC loader to handle cells that are numeric when being parsed.

--- codevalidator-api/src/main/java/org/sitenv/vocabularies/loader/valueset/vsac/VsacLoader.java
+++ codevalidator-api/src/main/java/org/sitenv/vocabularies/loader/valueset/vsac/VsacLoader.java
@@ -3,32 +3,21 @@ package org.sitenv.vocabularies.loader.valueset.vsac;
 import com.orientechnologies.common.io.OIOUtils;
 import com.orientechnologies.orient.core.intent.OIntentMassiveInsert;
 import com.orientechnologies.orient.core.sql.OCommandSQL;
-
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-
-import org.apache.commons.lang3.StringUtils;
+import com.orientechnologies.orient.object.db.OObjectDatabaseTx;
 import org.apache.commons.lang3.text.StrBuilder;
 import org.apache.log4j.Logger;
 import org.apache.poi.hssf.usermodel.HSSFSheet;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.poifs.filesystem.POIFSFileSystem;
+import org.apache.poi.ss.usermodel.Cell;
 import org.sitenv.vocabularies.constants.VocabularyConstants;
 import org.sitenv.vocabularies.loader.valueset.ValueSetLoader;
 import org.sitenv.vocabularies.loader.valueset.ValueSetLoaderManager;
-import org.sitenv.vocabularies.model.ValueSetModel;
-import org.sitenv.vocabularies.model.ValueSetModelDefinition;
-import org.sitenv.vocabularies.model.impl.LoincModel;
 import org.sitenv.vocabularies.model.impl.VsacValueSetModel;
 import org.sitenv.vocabularies.repository.VocabularyRepository;
 
-import com.orientechnologies.orient.object.db.OObjectDatabaseTx;
+import java.io.*;
+import java.util.List;
 
 public class VsacLoader implements ValueSetLoader {
 
@@ -46,13 +35,10 @@ public class VsacLoader implements ValueSetLoader {
 	
 
 	public void load(List<File> filesToLoad) {
-		
-
 		OObjectDatabaseTx dbConnection = VocabularyRepository.getInstance().getInactiveDbConnection();
 		HSSFWorkbook workBook = null;
 		
 		try {
-			
 			logger.info("Truncating VsacValueSetModel Datastore...");
 			VocabularyRepository.truncateValueSetModel(dbConnection, VsacValueSetModel.class);
 			logger.info(dbConnection.getName() + ".VsacValueSetModel Datastore Truncated... records remaining: " + VocabularyRepository.getValueSetRecordCount(dbConnection, VsacValueSetModel.class));
@@ -61,77 +47,73 @@ public class VsacLoader implements ValueSetLoader {
 		
 			String insertQueryPrefix = "insert into " + VsacValueSetModel.class.getSimpleName() +
 				" (valueSetIndex, valueSetNameIndex, codeSystemIndex, codeIndex, descriptionIndex, code, codeSystem, codeSystemName, codeSystemVersion, description, definitionVersion, steward, tty, type, valueSet, valueSetName) values ";
-			
 			StrBuilder insertQueryBuilder = new StrBuilder(insertQueryPrefix);
 			insertQueryBuilder.ensureCapacity(1000);
-			
 			int totalCount = 0, pendingCount = 0;
 			
 			dbConnection.declareIntent(new OIntentMassiveInsert());
 			
-			for (File file : filesToLoad)
-			{
-				if (file.isFile() && !file.isHidden())
-				{
-					
+			for (File file : filesToLoad) {
+				if (file.isFile() && !file.isHidden()) {
 					logger.debug("Loading Value Set File: " + file.getName());
 
 					InputStream inputStream = null;
-
-				
 					inputStream = new FileInputStream(file);
-				
-
 					POIFSFileSystem fileSystem = null;
-
-					
 					fileSystem = new POIFSFileSystem(inputStream);
-
 					workBook = new HSSFWorkbook(fileSystem);
 					HSSFSheet sheet = workBook.getSheet("Code List");
-						
-						
+
 					String valueSetName = ((String) OIOUtils.encode(sheet.getRow(1).getCell(1).getStringCellValue()));
 					String oid = ((String) OIOUtils.encode(sheet.getRow(2).getCell(1).getStringCellValue()));
 					String type = ((String) OIOUtils.encode(sheet.getRow(3).getCell(1).getStringCellValue()));
-					String version = ((String) OIOUtils.encode(sheet.getRow(4).getCell(1).getStringCellValue()));
+                    String version;
+                    Cell versionCell = sheet.getRow(4).getCell(1);
+                    versionCell.setCellType(Cell.CELL_TYPE_STRING);
+                    version = (String) OIOUtils.encode(versionCell.getStringCellValue());
 					String steward = ((String) OIOUtils.encode(sheet.getRow(5).getCell(1).getStringCellValue()));
-					
 					String valueSetIndex = oid.toUpperCase();
 					String valueSetNameIndex = valueSetName.toUpperCase();
-				
-					
+
 					for (int count = 11; count <= sheet.getLastRowNum(); count++)
 					{
 						if (pendingCount++ > 0) {
 							insertQueryBuilder.append(",");
 						}
-						
-						String code = (String) OIOUtils.encode(sheet.getRow(count).getCell(0).getStringCellValue());
-						String description = (String) OIOUtils.encode(sheet.getRow(count).getCell(1).getStringCellValue());
-						String codeSystemName = (String) OIOUtils.encode(sheet.getRow(count).getCell(2).getStringCellValue());
-						String codeSystemVersion = (String) OIOUtils.encode(sheet.getRow(count).getCell(3).getStringCellValue());
-						String codeSystem = (String) OIOUtils.encode(sheet.getRow(count).getCell(4).getStringCellValue());
-						String tty = (String) OIOUtils.encode(sheet.getRow(count).getCell(5).getStringCellValue());
+
+                        String code;
+                        String description;
+                        String codeSystemName;
+                        String codeSystemVersion;
+                        String codeSystem;
+                        String tty;
+
+                        Cell codeCell = sheet.getRow(count).getCell(0);
+                        Cell descriptionCell = sheet.getRow(count).getCell(1);
+                        Cell codeSystemNameCell = sheet.getRow(count).getCell(2);
+                        Cell codeSystemVersionCell = sheet.getRow(count).getCell(3);
+                        Cell codeSystemCell = sheet.getRow(count).getCell(4);
+                        Cell ttyCell = sheet.getRow(count).getCell(5);
+
+                        codeCell.setCellType(Cell.CELL_TYPE_STRING);
+                        code = (String) OIOUtils.encode(codeCell.getStringCellValue());
+                        description = (String) OIOUtils.encode(descriptionCell.getStringCellValue());
+                        codeSystemName = (String) OIOUtils.encode(codeSystemNameCell.getStringCellValue());
+                        codeSystemVersion = (String) OIOUtils.encode(codeSystemVersionCell.getStringCellValue());
+                        codeSystem = (String) OIOUtils.encode(codeSystemCell.getStringCellValue());
+                        tty = (String) OIOUtils.encode(ttyCell.getStringCellValue());
 						
 						insertQueryBuilder.append("(\"");
-						
 						insertQueryBuilder.append(valueSetIndex);
 						insertQueryBuilder.append("\",\"");
 						insertQueryBuilder.append(valueSetNameIndex);
 						insertQueryBuilder.append("\",\"");
-
 						insertQueryBuilder.append(codeSystem.toUpperCase());
 						insertQueryBuilder.append("\",\"");
-						
-
 						insertQueryBuilder.append(code.toUpperCase());
 						insertQueryBuilder.append("\",\"");
-						
-
 						insertQueryBuilder.append(description.toUpperCase());
 						insertQueryBuilder.append("\",\"");
-						
 						insertQueryBuilder.append(code);
 						insertQueryBuilder.append("\",\"");
 						insertQueryBuilder.append(codeSystem);
@@ -165,13 +147,9 @@ public class VsacLoader implements ValueSetLoader {
 							pendingCount = 0;
 						}
 					}
-					
 					workBook.close();
-					
-						
 				}
 			}
-			
 				if (pendingCount > 0) {
 					dbConnection.command(new OCommandSQL(insertQueryBuilder.toString())).execute();
 					dbConnection.commit();
@@ -197,7 +175,6 @@ public class VsacLoader implements ValueSetLoader {
 				Runtime r = Runtime.getRuntime();
 				r.gc();
 			}
-
 	}
 	
 	public String getValueSetAuthorName() {
