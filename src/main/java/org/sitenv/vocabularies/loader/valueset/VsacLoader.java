68357fbe53f2f4addfadc3681cfdc93eecb06a55
Brian Harris <sendbrianemail@gmail.com>
2016-12-28 17:16:29 -0500

    Added toUpper and trims to the extracted cell values. Forgot to add them last commit.

--- src/main/java/org/sitenv/vocabularies/loader/valueset/VsacLoader.java
+++ src/main/java/org/sitenv/vocabularies/loader/valueset/VsacLoader.java
@@ -2,7 +2,6 @@ package org.sitenv.vocabularies.loader.valueset;
 
 import com.monitorjbl.xlsx.StreamingReader;
 import org.apache.log4j.Logger;
-import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
@@ -29,81 +28,56 @@ public class VsacLoader extends BaseVocabularyLoader implements VocabularyLoader
             if (file.isFile() && !file.isHidden()) {
                 try {
                     logger.info("Loading Value Set File: " + file.getName());
-                    InputStream inputStream = new FileInputStream(file);
                     PreparedStatement preparedStatement = connection.prepareStatement(insertQueryPrefix);
+                    InputStream inputStream = new FileInputStream(file);
                     Workbook workBook = StreamingReader.builder().open(inputStream);
                     for (int i = 1; i < workBook.getNumberOfSheets(); i++) {
+
                         Sheet sheet = workBook.getSheetAt(i);
                         String valueSetName = "";
                         String valueSetOid = "";
                         String valueSetType = "";
                         String valueSetVersion = "";
                         String valueSetSteward = "";
+
                         for(Row row : sheet){
                             if(row.getRowNum() < 6) {
                                 if (row.getRowNum() == 1) {
-                                    valueSetName = row.getCell(1).getStringCellValue();
+                                    valueSetName = row.getCell(1).getStringCellValue().toUpperCase().trim();
                                 }
                                 if (row.getRowNum() == 2) {
-                                    valueSetOid = row.getCell(1).getStringCellValue();
+                                    valueSetOid = row.getCell(1).getStringCellValue().toUpperCase().trim();
                                 }
                                 if (row.getRowNum() == 3) {
-                                    valueSetType = row.getCell(1).getStringCellValue();
+                                    valueSetType = row.getCell(1).getStringCellValue().toUpperCase().trim();
                                 }
                                 if (row.getRowNum() == 4) {
-                                    valueSetVersion = row.getCell(1).getStringCellValue();
+                                    valueSetVersion = row.getCell(1).getStringCellValue().toUpperCase().trim();
                                 }
                                 if (row.getRowNum() == 5) {
-                                    valueSetSteward = row.getCell(1).getStringCellValue();
+                                    valueSetSteward = row.getCell(1).getStringCellValue().replaceAll("'", "''").toUpperCase().trim();
                                 }
                             }
 
                             if(row.getRowNum() > 10){
-                                String code = "";
-                                String displayName = "";
-                                String codeSystemName = "";
-                                String codeSystemVersion = "";
-                                String codeSystem = "";
-                                String tty = "";
-                                for(Cell cell : row){
-                                    if(row.getCell(cell.getColumnIndex()) != null){
-                                        if(cell.getColumnIndex() == 0){
-                                            code = row.getCell(0).getStringCellValue();
-                                        }
-                                        if(cell.getColumnIndex() == 1){
-                                            displayName = row.getCell(1).getStringCellValue();
-                                        }
-                                        if(cell.getColumnIndex() == 2){
-                                            codeSystemName = row.getCell(2).getStringCellValue();
-                                        }
-                                        if(cell.getColumnIndex() == 3){
-                                            codeSystemVersion = row.getCell(3).getStringCellValue();
-                                        }
-                                        if(cell.getColumnIndex() == 4){
-                                            codeSystem = row.getCell(4).getStringCellValue();
-                                        }
-                                        if(cell.getColumnIndex() == 5){
-                                            tty = row.getCell(5).getStringCellValue();
-                                        }
-                                    }
+                                if(row.getCell(0) != null) {
+                                    preparedStatement.setString(1, row.getCell(0).getStringCellValue().replaceAll("'", "''").toUpperCase().trim());
+                                    preparedStatement.setString(2, row.getCell(1).getStringCellValue().replaceAll("'", "''").toUpperCase().trim());
+                                    preparedStatement.setString(3, row.getCell(2).getStringCellValue().toUpperCase().trim());
+                                    preparedStatement.setString(4, row.getCell(3).getStringCellValue().trim());
+                                    preparedStatement.setString(5, row.getCell(4).getStringCellValue().toUpperCase().trim());
+                                    preparedStatement.setString(6, row.getCell(5).getStringCellValue().toUpperCase().trim());
+                                    preparedStatement.setString(7, valueSetName);
+                                    preparedStatement.setString(8, valueSetOid);
+                                    preparedStatement.setString(9, valueSetType);
+                                    preparedStatement.setString(10, valueSetVersion);
+                                    preparedStatement.setString(11, valueSetSteward);
+                                    preparedStatement.addBatch();
                                 }
-
-                                preparedStatement.setString(1, code);
-                                preparedStatement.setString(2, displayName);
-                                preparedStatement.setString(3, codeSystemName);
-                                preparedStatement.setString(4, codeSystemVersion);
-                                preparedStatement.setString(5, codeSystem);
-                                preparedStatement.setString(6, tty);
-                                preparedStatement.setString(7, valueSetName);
-                                preparedStatement.setString(8, valueSetOid);
-                                preparedStatement.setString(9,valueSetType);
-                                preparedStatement.setString(10, valueSetVersion);
-                                preparedStatement.setString(11, valueSetSteward);
-                                preparedStatement.addBatch();
-
                                 if(row.getRowNum() % 1000 == 0){
                                     preparedStatement.executeBatch();
                                     connection.commit();
+                                    preparedStatement.clearBatch();
                                 }
                             }
                         }
