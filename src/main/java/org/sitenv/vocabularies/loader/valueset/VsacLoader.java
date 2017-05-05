3f38e773995a7acc9032012808e2d0a9c13e8c4b
Brian Harris <sendbrianemail@gmail.com>
2017-05-05 15:40:12 -0400

    VSAC loader now looks for the header row by looking for a cell at index 0 with the toUpperCase value of CODE. This allows for the header row to start on any row below the top 6 metadata rows. VSAC loader now checks the to see if the current row is valid for processing by determining if the row has at least 6 cells and the first cell is not null and empty.

--- src/main/java/org/sitenv/vocabularies/loader/valueset/VsacLoader.java
+++ src/main/java/org/sitenv/vocabularies/loader/valueset/VsacLoader.java
@@ -21,6 +21,10 @@ import java.util.List;
 @Component(value = "VSAC")
 public class VsacLoader extends BaseCodeLoader implements VocabularyLoader {
     private static Logger logger = Logger.getLogger(VsacLoader.class);
+    private static final int MIN_EXPECTED_NUMBER_OF_CELLS_IN_ROW = 6;
+    private static final int CODE_CELL_INDEX_IN_ROW = 0;
+    private static final int BATCH_SIZE = 1000;
+    private static final String HEADER_ROW_FINDER_KEY = "CODE";
 
     public void load(List<File> filesToLoad, Connection connection) {
         String insertQueryPrefix = "insert into VALUESETS (ID, CODE, DISPLAYNAME, CODESYSTEMNAME, CODESYSTEMVERSION, CODESYSTEM, TTY, VALUESETNAME, VALUESETOID, VALUESETTYPE, VALUESETDEFINITIONVERSION, VALUESETSTEWARD) values (DEFAULT ,?,?,?,?,?,?,?,?,?,?,?)";
@@ -32,13 +36,14 @@ public class VsacLoader extends BaseCodeLoader implements VocabularyLoader {
                     InputStream inputStream = new FileInputStream(file);
                     Workbook workBook = StreamingReader.builder().open(inputStream);
                     for (int i = 1; i < workBook.getNumberOfSheets(); i++) {
-
+                        boolean headerRowFound = false;
                         Sheet sheet = workBook.getSheetAt(i);
                         String valueSetName = "";
                         String valueSetOid = "";
                         String valueSetType = "";
                         String valueSetVersion = "";
                         String valueSetSteward = "";
+                        int valuesetDataRowCount = 0;
 
                         for(Row row : sheet){
                             if(row.getRowNum() < 6) {
@@ -59,27 +64,35 @@ public class VsacLoader extends BaseCodeLoader implements VocabularyLoader {
                                 }
                             }
 
-                            if(row.getRowNum() > 10){
-                                preparedStatement.setString(1, hasCellData(row, 0) ? row.getCell(0).getStringCellValue().toUpperCase().trim() : "");
-                                preparedStatement.setString(2, hasCellData(row, 1) ? row.getCell(1).getStringCellValue().toUpperCase().trim() : "");
-                                preparedStatement.setString(3, hasCellData(row, 2) ? row.getCell(2).getStringCellValue().toUpperCase().trim() : "");
-                                preparedStatement.setString(4, hasCellData(row, 3) ? row.getCell(3).getStringCellValue().trim() : "");
-                                preparedStatement.setString(5, hasCellData(row, 4) ? row.getCell(4).getStringCellValue().toUpperCase().trim() : "");
-                                preparedStatement.setString(6, hasCellData(row, 5) ? row.getCell(5).getStringCellValue().toUpperCase().trim() : "");
+                            if(headerRowFound && canProcessRow(row)){
+                                preparedStatement.setString(1, row.getCell(0).getStringCellValue().toUpperCase().trim());
+                                preparedStatement.setString(2, row.getCell(1).getStringCellValue().toUpperCase().trim());
+                                preparedStatement.setString(3, row.getCell(2).getStringCellValue().toUpperCase().trim());
+                                preparedStatement.setString(4, row.getCell(3).getStringCellValue().trim());
+                                preparedStatement.setString(5, row.getCell(4).getStringCellValue().toUpperCase().trim());
+                                preparedStatement.setString(6, row.getCell(5).getStringCellValue().toUpperCase().trim());
                                 preparedStatement.setString(7, valueSetName);
                                 preparedStatement.setString(8, valueSetOid);
                                 preparedStatement.setString(9, valueSetType);
                                 preparedStatement.setString(10, valueSetVersion);
                                 preparedStatement.setString(11, valueSetSteward);
                                 preparedStatement.addBatch();
+                                valuesetDataRowCount++;
 
-                                if(row.getRowNum() % 1000 == 0){
+                                if(valuesetDataRowCount % BATCH_SIZE == 0){
                                     preparedStatement.executeBatch();
                                     connection.commit();
                                     preparedStatement.clearBatch();
                                 }
                             }
+
+                            if(!headerRowFound){
+                                if(hasValueInCell(row, 0) && row.getCell(0).getStringCellValue().toUpperCase().trim().equals(HEADER_ROW_FINDER_KEY)){
+                                    headerRowFound = true;
+                                }
+                            }
                         }
+
                         preparedStatement.executeBatch();
                         connection.commit();
                     }
@@ -92,7 +105,19 @@ public class VsacLoader extends BaseCodeLoader implements VocabularyLoader {
         }
     }
 
-    private boolean hasCellData(Row row, int cellNum) {
+    private boolean canProcessRow(Row row) {
+        return hasCodevalueInFirstCell(row) && hasExpectedNumberOfCellsInRow(row);
+    }
+
+    private boolean hasCodevalueInFirstCell(Row row){
+        return hasValueInCell(row, CODE_CELL_INDEX_IN_ROW);
+    }
+
+    private boolean hasExpectedNumberOfCellsInRow(Row row) {
+        return row.getLastCellNum() >= MIN_EXPECTED_NUMBER_OF_CELLS_IN_ROW;
+    }
+
+    private boolean hasValueInCell(Row row, int cellNum) {
         return row.getCell(cellNum) != null && !row.getCell(cellNum).getStringCellValue().isEmpty();
     }
 }
