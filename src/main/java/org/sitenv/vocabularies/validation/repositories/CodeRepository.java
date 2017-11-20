8769d1291caafc5a607895d26df50812598c50ff
drbgfc <drb.gfc@comcast.net>
2017-11-20 17:48:28 -0500

    Fix Query to select active data and further handle duplicates with Sets

--- src/main/java/org/sitenv/vocabularies/validation/repositories/CodeRepository.java
+++ src/main/java/org/sitenv/vocabularies/validation/repositories/CodeRepository.java
@@ -7,6 +7,7 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Set;
 
 /**
  * Created by Brian on 2/7/2016.
@@ -14,25 +15,25 @@ import java.util.List;
 @Repository
 public interface CodeRepository extends JpaRepository<Code, Integer> {
     @Query("SELECT CASE WHEN COUNT(c) > 0 THEN true ELSE false END FROM Code c WHERE c.codeSystem in (:codesystems)")
-    boolean foundCodesystems(@Param("codesystems")List<String> codesystems);
+    boolean foundCodesystems(@Param("codesystems")Set<String> codesystems);
 
     @Query("SELECT CASE WHEN COUNT(c) > 0 THEN true ELSE false END FROM Code c WHERE c.code = :code and c.displayName = :displayName and c.codeSystem in (:codesystems)")
     boolean foundCodeAndDisplayNameInCodesystem(@Param("code")String code, @Param("displayName")String displayName, @Param("codesystems")List<String> codesystems);
 
     @Query("SELECT CASE WHEN COUNT(c) > 0 THEN true ELSE false END FROM Code c WHERE c.code = :code and c.displayName = :displayName and c.codeSystemOID = :codeSystemOID and c.codeSystem in (:codesystems) and c.active = true")
-    boolean foundActiveCodeAndDisplayNameAndCodeSystemOIDInCodesystem(@Param("code")String code, @Param("displayName")String displayName, @Param("codeSystemOID")String codeSystemOID, @Param("codesystems")List<String> codesystems);
+    boolean foundActiveCodeAndDisplayNameAndCodeSystemOIDInCodesystem(@Param("code")String code, @Param("displayName")String displayName, @Param("codeSystemOID")String codeSystemOID, @Param("codesystems")Set<String> codesystems);
 
     @Query("SELECT CASE WHEN COUNT(c) > 0 THEN true ELSE false END FROM Code c WHERE c.code = :code and c.codeSystem in (:codesystems)")
-    boolean foundCodeInCodesystems(@Param("code")String code, @Param("codesystems")List<String> codesystems);
+    boolean foundCodeInCodesystems(@Param("code")String code, @Param("codesystems")Set<String> codesystems);
 
     @Query("SELECT CASE WHEN COUNT(c) > 0 THEN true ELSE false END FROM Code c WHERE c.displayName = :displayName and c.codeSystem in (:codesystems)")
-    boolean foundDisplayNameInCodesystems(@Param("displayName")String displayName, @Param("codesystems")List<String> codesystems);
+    boolean foundDisplayNameInCodesystems(@Param("displayName")String displayName, @Param("codesystems")Set<String> codesystems);
 
     @Query("SELECT CASE WHEN COUNT(c) > 0 THEN true ELSE false END FROM Code c WHERE c.codeSystemOID = :codeSystemOID and c.codeSystem in (:codesystems)")
-    boolean foundCodeSystemOIDInCodesystems(@Param("codeSystemOID")String codeSystemOID, @Param("codesystems")List<String> codesystems);
+    boolean foundCodeSystemOIDInCodesystems(@Param("codeSystemOID")String codeSystemOID, @Param("codesystems")Set<String> codesystems);
 
     List<Code> findByCodeAndCodeSystemIn(String code, List<String> codesystems);
 
-    @Query("SELECT c.active FROM Code c WHERE c.code = :code and c.codeSystem in (:codesystems)")
-    boolean codeIsActive(@Param("code")String code, @Param("codesystems")List<String> codesystems);
+    @Query("SELECT c.active FROM Code c WHERE c.active = 1 AND c.code = :code and c.codeSystem in (:codesystems)")
+    boolean codeIsActive(@Param("code")String code, @Param("codesystems")Set<String> codesystems);
 }
