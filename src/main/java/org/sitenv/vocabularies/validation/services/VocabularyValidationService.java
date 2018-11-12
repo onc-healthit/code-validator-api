e0ebf7eb9eb3fd37b9a1c1fe2d95d267949c797a
drbgfc <drb.gfc@comcast.net>
2018-11-12 17:50:16 -0500

    Implement/Add a testing strategy for the VocabularyValidationService
    -Currently supports the (newly added) RequiredNodeValidator via a
    programmatic configuration but is intended to be extendable to other
    validators using programmatic or file-based configs.

--- src/main/java/org/sitenv/vocabularies/validation/services/VocabularyValidationService.java
+++ src/main/java/org/sitenv/vocabularies/validation/services/VocabularyValidationService.java
@@ -1,5 +1,23 @@
 package org.sitenv.vocabularies.validation.services;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+
+import javax.annotation.Resource;
+import javax.servlet.ServletContext;
+import javax.xml.namespace.NamespaceContext;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathExpressionException;
+import javax.xml.xpath.XPathFactory;
+
 import org.apache.log4j.Logger;
 import org.sitenv.vocabularies.configuration.CodeValidatorApiConfiguration;
 import org.sitenv.vocabularies.configuration.Configurations;
@@ -19,20 +37,6 @@ import org.w3c.dom.Document;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
-
-import javax.annotation.Resource;
-import javax.servlet.ServletContext;
-import javax.xml.namespace.NamespaceContext;
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.xpath.XPath;
-import javax.xml.xpath.XPathConstants;
-import javax.xml.xpath.XPathExpressionException;
-import javax.xml.xpath.XPathFactory;
-
-import java.io.File;
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.*;
 
 /**
  * Created by Brian on 2/10/2016.
@@ -93,35 +97,7 @@ public class VocabularyValidationService {
                 }
                 
                 if (vocabularyValidationConfigurations != null) {
-	                for (ConfiguredExpression configuredExpression : vocabularyValidationConfigurations) {
-	                    configuredXpathExpression = configuredExpression.getConfiguredXpathExpression();
-	                    NodeList nodes = findAllDocumentNodesByXpathExpression(xpath, configuredXpathExpression, doc);
-	                    for (int i = 0; i < nodes.getLength(); i++) {
-	                        Node node = nodes.item(i);
-	                        List<VocabularyValidationResult> vocabularyValidationResults = new ArrayList<>();
-	                        boolean validNode = false;
-	                        Iterator configIterator = configuredExpression.getConfiguredValidators().iterator();
-	                        while(configIterator.hasNext() && !validNode){
-	                            ConfiguredValidator configuredValidator = (ConfiguredValidator) configIterator.next();
-	                            NodeValidation vocabularyValidator = vocabularyValidatorFactory.getVocabularyValidator(configuredValidator.getName());
-	                            List<VocabularyValidationResult> tempResults = vocabularyValidator.validateNode(configuredValidator, xpath, node, i);
-	                            if(foundValidationError(tempResults)) {
-	                                vocabularyValidationResults.addAll(tempResults);
-	                            }else {
-	                                vocabularyValidationResults.clear();
-	                                vocabularyValidationResults.addAll(tempResults);
-	                                validNode = true;
-	                            }
-	                        }
-	
-	                        for (VocabularyValidationResult vocabularyValidationResult : vocabularyValidationResults) {
-	                            vocabularyValidationResult.getNodeValidationResult().setConfiguredXpathExpression(configuredXpathExpression);
-	                            vocabularyValidationResultMap.get(vocabularyValidationResult.getVocabularyValidationResultLevel().getResultType()).add(vocabularyValidationResult);
-	                        }
-	
-	                    }
-	
-	                }
+                	validate(vocabularyValidationResultMap, configuredXpathExpression, xpath, doc);
                 } else {
                 	logger.error("Vocabulary validation was not run as vocabularyValidationConfigurations is null");
                 }
@@ -131,6 +107,43 @@ public class VocabularyValidationService {
         }
         return convertMapToList(vocabularyValidationResultMap);
     }
+    
+	private void validate(Map<String, ArrayList<VocabularyValidationResult>> vocabularyValidationResultMap,
+			String configuredXpathExpression, XPath xpath, Document doc) throws XPathExpressionException {
+        for (ConfiguredExpression configuredExpression : vocabularyValidationConfigurations) {
+            configuredXpathExpression = configuredExpression.getConfiguredXpathExpression();
+            NodeList nodes = findAllDocumentNodesByXpathExpression(xpath, configuredXpathExpression, doc);
+            for (int i = 0; i < nodes.getLength(); i++) {
+                Node node = nodes.item(i);
+                List<VocabularyValidationResult> vocabularyValidationResults = new ArrayList<>();
+                boolean validNode = false;
+                Iterator configIterator = configuredExpression.getConfiguredValidators().iterator();
+                while(configIterator.hasNext() && !validNode){
+                    ConfiguredValidator configuredValidator = (ConfiguredValidator) configIterator.next();
+                    NodeValidation vocabularyValidator = selectVocabularyValidator(configuredValidator);
+                    List<VocabularyValidationResult> tempResults = vocabularyValidator.validateNode(configuredValidator, xpath, node, i);
+                    if(foundValidationError(tempResults)) {
+                        vocabularyValidationResults.addAll(tempResults);
+                    }else {
+                        vocabularyValidationResults.clear();
+                        vocabularyValidationResults.addAll(tempResults);
+                        validNode = true;
+                    }
+                }
+
+                for (VocabularyValidationResult vocabularyValidationResult : vocabularyValidationResults) {
+                    vocabularyValidationResult.getNodeValidationResult().setConfiguredXpathExpression(configuredXpathExpression);
+                    vocabularyValidationResultMap.get(vocabularyValidationResult.getVocabularyValidationResultLevel().getResultType()).add(vocabularyValidationResult);
+                }
+
+            }
+
+        }
+    }
+	
+	protected NodeValidation selectVocabularyValidator(ConfiguredValidator configuredValidator) {
+		return vocabularyValidatorFactory.getVocabularyValidator(configuredValidator.getName());
+	}
     
     private boolean useDynamicVocab(String vocabularyConfig) {
     	logger.info("Attempting to overwrite pre-loaded vocabulary configuration dynamically "
---
+++ src/test/java/TestableVocabularyValidationService.java
@@ -0,0 +1,20 @@
+import org.sitenv.vocabularies.configuration.ConfiguredValidator;
+import org.sitenv.vocabularies.validation.NodeValidation;
+import org.sitenv.vocabularies.validation.services.VocabularyValidationService;
+
+public class TestableVocabularyValidationService extends VocabularyValidationService {
+
+	@Override
+	protected NodeValidation selectVocabularyValidator(ConfiguredValidator configuredValidator) {
+		NodeValidation vocabularyValidator = null;
+		final String packagePath = "org.sitenv.vocabularies.validation.validators.nodetypes.";
+		try {
+			vocabularyValidator = (NodeValidation) Class.forName(packagePath + configuredValidator.getName())
+					.newInstance();
+		} catch (InstantiationException | IllegalAccessException | ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+		return vocabularyValidator;
+	}
+
+}
